86ea56a7d76ca5e48bc404e069415b4512354851
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-11 13:28:54 +0200

    displayProducts looping

--- displayProducts.js
+++ displayProducts.js
@@ -1,14 +1,18 @@
+import { generateProductCard } from "./generateProductCard.js";
+
 /**
  * Display product cards in the display container.
  * @param {Array} products Array of JSON product objects.
  */
 function displayProducts(products) {
-  console.log(products);
-
   const displayContainer = document.getElementById("display-container");
 
   // Clear Container
   displayContainer.textContent = "";
+
+  products.forEach((product) =>
+    displayContainer.appendChild(generateProductCard(product))
+  );
 }
 
 export { displayProducts };
