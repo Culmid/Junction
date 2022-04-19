80f71ca8f9b0b6436e9adcb155bac65e482adc2b
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-19 16:08:53 +0200

    Clean displayDropdown.js

--- scripts/displays/displayDropdown.js
+++ scripts/displays/displayDropdown.js
@@ -9,14 +9,12 @@ import { formatPrice } from "../utilities/utils.js";
 function displayDropdown() {
   const dropdownList = document.getElementById("dropdown-list");
 
-  // Clear Dropdown
   dropdownList.innerHTML = "";
 
   const cartList = getCartList();
   cartList.forEach((item) =>
     dropdownList.appendChild(generateDropdownCard(item))
   );
-  console.log(cartList);
 
   const cartDropdownCount = document.getElementById("cart-dropdown-count");
   cartDropdownCount.lastChild.textContent = " " + getCartCount();
