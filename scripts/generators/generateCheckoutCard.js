ff3d51d1accdb3e274a05bc049bc394396281121
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-19 18:34:36 +0200

    Refactor generateCheckoutCard.js

--- scripts/generators/generateCheckoutCard.js
+++ scripts/generators/generateCheckoutCard.js
@@ -2,45 +2,108 @@ import { displayCheckout } from "../displays/displayCheckout.js";
 import { removeByIndex } from "../utilities/shoppingCart.js";
 import { formatPrice } from "../utilities/utils.js";
 
+/**
+ * Generate a checkout page item card.
+ * @param {Object} product Product Object.
+ * @param {number} index Index of product in cart.
+ * @returns HTMLElement containing the checkout card.
+ */
 function generateCheckoutCard(product, index) {
   const checkoutItem = document.createElement("div");
   checkoutItem.classList.add("checkout-item");
 
+  checkoutItem.appendChild(checkoutItemLeft(product));
+  checkoutItem.appendChild(checkoutItemRight(product, index));
+
+  return checkoutItem;
+}
+
+/**
+ * Generate the left div of the checkout card.
+ * @param {Object} product Product object with image and name extracted.
+ * @returns HTMLElement containing the left elements of the card.
+ */
+function checkoutItemLeft({ image, name }) {
   const checkoutItemLeft = document.createElement("div");
   checkoutItemLeft.classList.add("checkout-item-left");
-  checkoutItem.appendChild(checkoutItemLeft);
 
+  checkoutItemLeft.appendChild(itemImage(image));
+  checkoutItemLeft.appendChild(itemHeader(name));
+
+  return checkoutItemLeft;
+}
+
+/**
+ * Generate the image element for this checkout item.
+ * @param {string} image Path to the image for this item.
+ * @returns HTMLElement containing the image for this item.
+ */
+function itemImage(image) {
   const itemImg = document.createElement("img");
-  itemImg.src = product.image;
+  itemImg.src = image;
   itemImg.alt = "Item";
-  // Explicit Height/Width
   itemImg.style.height = "100px";
   itemImg.style.width = "100px";
-  checkoutItemLeft.appendChild(itemImg);
 
+  return itemImg;
+}
+
+/**
+ * Generate the header for this item.
+ * @param {string} name Name of the current item.
+ * @returns HTMLElement of item header.
+ */
+function itemHeader(name) {
   const itemHeader = document.createElement("h3");
-  itemHeader.innerHTML = product.name;
-  checkoutItemLeft.appendChild(itemHeader);
+  itemHeader.innerHTML = name;
 
+  return itemHeader;
+}
+
+/**
+ * Generate the right div in the checkout card.
+ * @param {Object} product Product object with discounted_price extracted.
+ * @param {number} index Index of the product in the cart.
+ * @returns HTMLElement containing the right div in the checkout card.
+ */
+function checkoutItemRight({ discounted_price }, index) {
   const checkoutItemRight = document.createElement("div");
   checkoutItemRight.classList.add("checkout-item-right");
-  checkoutItem.appendChild(checkoutItemRight);
 
+  checkoutItemRight.appendChild(checkoutItemPrice(discounted_price));
+  checkoutItemRight.appendChild(removeButton(index));
+
+  return checkoutItemRight;
+}
+
+/**
+ * Generate the checkout item price for the checkout card.
+ * @param {number} discountedPrice Lowest price of the item currently.
+ * @returns HTMLElement containing the checkout item price.
+ */
+function checkoutItemPrice(discountedPrice) {
   const checkoutItemPrice = document.createElement("div");
   checkoutItemPrice.classList.add("checkout-item-price");
-  checkoutItemPrice.innerHTML = formatPrice(product.discounted_price);
-  checkoutItemRight.appendChild(checkoutItemPrice);
+  checkoutItemPrice.innerHTML = formatPrice(discountedPrice);
 
+  return checkoutItemPrice;
+}
+
+/**
+ * Generate the remove button for the checkout card.
+ * @param {number} index Index of the item in the cart.
+ * @returns HTMLElement containing the remove button.
+ */
+function removeButton(index) {
   const removeButton = document.createElement("button");
   removeButton.innerHTML = "Remove";
-  checkoutItemRight.appendChild(removeButton);
 
   removeButton.addEventListener("click", () => {
     removeByIndex(index);
     displayCheckout();
   });
 
-  return checkoutItem;
+  return removeButton;
 }
 
 export { generateCheckoutCard };
