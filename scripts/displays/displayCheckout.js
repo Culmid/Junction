480a8274ee3e945296682e6582fe4c1762c5d07d
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-20 18:02:46 +0200

    rename sf

--- scripts/displays/displayCheckout.js
+++ scripts/displays/displayCheckout.js
@@ -6,7 +6,7 @@ import {
   getCartTotal,
 } from "../utilities/shoppingCart.js";
 import { calculateVAT, formatPrice } from "../utilities/utils.js";
-import { _r_e_t_s_a_e__g_g_e } from "../misc/_t_e_r_c_e_s__e_l_i_f_.js";
+import { _r_e_t_s_a_e__g_g_e } from "../misc/t_e_r_c_e_s__e_l_i_f.js";
 
 const onPay = () => _r_e_t_s_a_e__g_g_e();
 
