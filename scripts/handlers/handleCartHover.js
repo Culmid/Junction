ff548886972541a491e07bdc355fe5ff1bfc5eae
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-20 11:55:18 +0200

    Refactor handleCartHover.js

--- scripts/handlers/handleCartHover.js
+++ scripts/handlers/handleCartHover.js
@@ -2,30 +2,30 @@ import { displayCheckout } from "../displays/displayCheckout.js";
 import { displayDropdown } from "../displays/displayDropdown.js";
 import { clearCart } from "../utilities/shoppingCart.js";
 
+const dropdown = document.getElementById("cart-dropdown");
+
+const enterFunc = () => {
+  displayDropdown();
+  dropdown.style.display = "inline";
+};
+
+const leaveFunc = () => {
+  dropdown.style.display = "none";
+};
+
 function handleCartHover() {
-  const dropdown = document.getElementById("cart-dropdown");
-  dropdown.addEventListener("mouseenter", () => {
-    dropdown.style.display = "inline";
-  });
-  dropdown.addEventListener("mouseleave", () => {
-    dropdown.style.display = "none";
-  });
+  dropdown.addEventListener("mouseenter", enterFunc);
+  dropdown.addEventListener("mouseleave", leaveFunc);
 
   const cart = document.getElementById("cart");
-  cart.addEventListener("mouseenter", () => {
-    displayDropdown();
-    dropdown.style.display = "inline";
-  });
-  cart.addEventListener("mouseleave", () => {
-    dropdown.style.display = "none";
-  });
+  cart.addEventListener("mouseenter", enterFunc);
+  cart.addEventListener("mouseleave", leaveFunc);
 
   const clear = document.getElementById("clear-cart");
   clear.addEventListener("click", () => {
     clearCart();
     displayDropdown();
 
-    // On Checkout Page -> Synchronous Update
     if (window.location.pathname.includes("checkout.html")) {
       displayCheckout();
     }
